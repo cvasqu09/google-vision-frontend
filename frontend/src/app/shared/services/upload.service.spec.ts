adee6d970da27c19fc0a069d9317850e1f740774
Chris.V <cvasqu09@gmail.com>
2019-07-04 13:19:38 -0400

    Updated the upload service and added tests

--- frontend/src/app/shared/services/upload.service.spec.ts
+++ frontend/src/app/shared/services/upload.service.spec.ts
@@ -1,12 +1,47 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UploadService } from './upload.service';
+import createSpyObj = jasmine.createSpyObj;
+import { Upload } from '../../modules/upload/models/upload.model';
 
 describe('UploadService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: UploadService;
+  const putSpy = createSpyObj('child', ['child', 'put']);
+  const mockFile = createSpyObj('File', ['name']);
+  const mockFireStorage: any = {
+    storage: {
+      ref: () => {
+        return {
+          child: () => putSpy
+        };
+      }
+    }
+  };
+  const mockNotificationService = createSpyObj('NotificationService', ['notify']);
+
+  beforeEach(() => {
+    service = new UploadService(mockFireStorage, mockNotificationService);
+  });
 
   it('should be created', () => {
-    const service: UploadService = TestBed.get(UploadService);
     expect(service).toBeTruthy();
+  });
+
+  it('should upload file', async () => {
+    putSpy.put.and.returnValue(Promise.resolve('resolved'));
+    mockFile.name = 'test.txt';
+    await service.upload(new Upload(mockFile));
+
+    expect(putSpy.put).toHaveBeenCalled();
+    expect(mockNotificationService.notify).toHaveBeenCalledWith('Successfully uploaded test.txt');
+  });
+
+  it('should notify when an error occurs', async () => {
+    putSpy.put.and.returnValue(Promise.reject('boom'));
+    mockFile.name = 'test.txt';
+    await service.upload(new Upload(mockFile));
+
+    expect(putSpy.put).toHaveBeenCalled();
+    expect(mockNotificationService.notify).toHaveBeenCalledWith('Failed to upload test.txt');
   });
 });
--- frontend/src/app/shared/services/upload.service.ts
+++ frontend/src/app/shared/services/upload.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { Upload } from '../../modules/upload/models/upload.model';
+import { NotificationService } from './notification.service';
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
+  constructor(private fireStorage: AngularFireStorage, private notificationService: NotificationService) { }
 
-  constructor(private fireStorage: AngularFireStorage) { }
-
-  upload(upload: Upload) {
+  async upload(upload: Upload) {
     this.fireStorage.storage.ref();
-    const testRef = this.fireStorage.storage.ref().child(`${upload.name}.txt`);
-    testRef.put(upload.file).then((snapshot) => {
-      console.log('uploaded file', snapshot);
+    const testRef = this.fireStorage.storage.ref().child(`${upload.name}`);
+    return testRef.put(upload.file).then((snapshot) => {
+      this.notificationService.notify(`Successfully uploaded ${upload.name}`);
     }).catch((err) => {
-      console.log('Error while uploading file', err);
+      this.notificationService.notify(`Failed to upload ${upload.name}`);
     });
   }
 }
